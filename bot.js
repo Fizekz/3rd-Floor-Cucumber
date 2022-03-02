04ad7cc26c86d040697d381d1b9f986af1de9b29
Fizekz <100499669+Fizekz@users.noreply.github.com>
2022-03-02 14:18:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
-const { MessageEmbed } = require('discord.js');
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const BOT_PREFIX = "c!"
 const RANDOM_ROLE_COMMAND = "random-role"
